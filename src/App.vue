2e2271ac799baa6652fcbfc1c5e18a4bf19f479d
hemantdubeyhd <hemantdubeyhd@gmail.com>
2025-08-06 11:57:53 +0930

    some practice using vue's Option Api.
    after this all new program of vue, we will try composition APi.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,20 @@
 export default {
   data() {
     return {
-      name: 'Hemmie'
+      name: 'Hemmie',
+      isLoggedIn: false
     };
+  },
+  methods:
+  {
+    toggleLogin(){
+      this.isLoggedIn = !this.isLoggedIn;
+    },
+    chnageName(){
+      this.name = 'Hemant Dubey';
+    }
   }
+  
 };
 </script>
 
@@ -12,6 +23,12 @@ export default {
   <div>
     <h1>Hello, {{ name }}!</h1>
     <input v-model="name" placeholder="Type your name" />
+
+    <p v-if="isLoggedIn">{{name}}, You are logged in</p>
+    <p v-else>{{name}}, You are logged out!</p>
+
+    <button @click="toggleLogin">Toggle Login</button>
+    <button @click="chnageName">Set name to 'Hemant Dubey'</button>
   </div>
 </template>
 
